39559ebedc8225ba37d0855a396bd8d9c35cd691
sociocreatives <ayaraerick@gmail.com>
2025-03-15 17:05:59 +0300

    login syustem

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -1,9 +1,23 @@
+import HomePageSearch from "@/components/forms/onboarding/HomePageSearch";
+import Image from "next/image";
 
 
 export default function Home() {
   return (
-    <div className=''>
-     <p>Home Page</p>
+    <div className='bg-primary md:mt-20 md:rounded-3xl rounded-xl'>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="flex flex-col gap-4 justify-center items-start md:p-20">
+          <small className="text-yellow-300 text-sm font-bold bg-green-900 px-4 py-2 rounded-3xl self-center md:self-start mt-10 md:mt-0">Get the right local Pro</small>
+          <h1 className="font-black md:text-5xl text-3xl text-white px-6 md:px-0 text-center md:text-left mb-6 md:mb-0">
+            Get the right local Pro
+            in your area for any project.
+          </h1>
+          <HomePageSearch/>
+        </div>
+        <div>
+          <Image src={'/images/kimpeople.png'} alt={'Mr_Kim_Logo'} width={500} height={100} className='self-center -mt-15 hidden md:block' />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/forms/onboarding/HomePageSearch.tsx
@@ -0,0 +1,15 @@
+
+import { Input } from '@/components/ui/input'
+import { SearchIcon } from 'lucide-react'
+import React from 'react'
+
+export default function HomePageSearch() {
+    return (
+        <div className='w-full md:mt-6 mt-1'>
+        <div className="flex flex-row justify-center items-center gap-2 bg-white p-2 rounded-lg mx-6 md:mx-0">
+            <div className='flex w-full justify-center items-center gap-2'><SearchIcon className="text-muted-foreground" />
+            <Input type="text" placeholder="Search for a Job" className="w-full border-none shadow-none outline-none" /></div>
+        </div>
+        </div>
+    )
+}
