5c62bc90fef5d4bf438960c578000d202d3b1f12
sociocreatives <ayaraerick@gmail.com>
2025-03-18 23:37:12 +0300

    categories

--- src/components/general/UserProfilePage.tsx
+++ src/components/general/UserProfilePage.tsx
@@ -1,7 +1,6 @@
 
 
 import { auth } from "@/lib/auth";
-import Image from "next/image";
 
 export default async function UserProfilePage() {
     const session  = await auth();
@@ -14,7 +13,6 @@ export default async function UserProfilePage() {
         <h1>User Profile</h1>
         <p><strong>Name:</strong> {session.user?.name}</p>
         <p><strong>Email:</strong> {session.user?.email}</p>
-        <Image src={session.user?.image} alt="Profile Image" width={100} height={100} className="rounded-full" />
         </>
     )
 }
