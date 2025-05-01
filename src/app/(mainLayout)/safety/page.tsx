3af71d08ebe484099c365e24c504605282367444
thekimpeople <thekimpeople@gmail.com>
2025-05-01 18:46:42 +0300

    finance

--- src/app/(mainLayout)/safety/page.tsx
+++ src/app/(mainLayout)/safety/page.tsx
@@ -1,15 +1,14 @@
 
-
 import Image from 'next/image'
 import React from 'react'
 
-
-export default function SignUpAsAPro() {
+export default function SafetyPage() {
     return (
         <>
             <div>
-                <Image src={'/images/finance.jpg'} alt={'Mr_Kim_Logo'} width={500} height={100} quality={100}  className='w-full rounded-lg mt-4' />
+                <Image src={'/images/safety.jpg'} alt={'Mr_Kim_Logo'} width={500} height={100} quality={100}  className='w-full rounded-lg mt-4' />
                 <div className='py-10'>
+zxvssadf
                 </div>
 
             </div>
--- src/components/general/Footer.tsx
+++ src/components/general/Footer.tsx
@@ -1,3 +1,4 @@
+
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -22,6 +23,15 @@ export default function Footer() {
                             <li className='text-white border-b-1 border-zinc-600 py-2 text-sm '><Link href='/howitworks'>How it Works</Link></li>
                             <li className='text-white border-b-1 border-zinc-600 py-2 text-sm'><Link href='/getapp'>Get the App</Link></li>
                             <li className='text-white border-b-1 border-zinc-600 py-2 text-sm'><Link href='/whychooseus'>Why choose Mr. Kim</Link></li>
+                            <li className='text-white border-b-1 border-zinc-600 py-2 text-sm'>
+                                <Link href='/cleaning' className='inline-flex items-center space-x-2 group'>
+                                    <span className="text-white relative">
+                                        Cleaning
+                                        <span className="absolute -top-1 -right-10 bg-green-600 text-white text-xs font-bold px-2 py-0.25 rounded-full">
+                                            <p className='text-xs'>New</p>
+                                        </span>
+                                    </span>
+                                </Link></li>
                         </ul>
                     </div>
                     <div>
@@ -31,7 +41,7 @@ export default function Footer() {
                             <li className='text-white border-b-1 border-zinc-600 py-2 text-sm'><Link href='/solutions'>Solutions Center & Expert Tips</Link></li>
                             <li className='text-white border-b-1 border-zinc-600 py-2 text-sm'><Link href='/sign_up_as_a_pro'>Sign up as a Pro</Link></li>
                             <li className='text-white border-b-1 border-zinc-600 py-2 text-sm'><Link href='/legal'>Legal</Link></li>
-                            <li className='text-white border-b-1 border-zinc-600 py-2 text-sm'><Link href='/solutions'>Safety</Link></li>
+                            <li className='text-white border-b-1 border-zinc-600 py-2 text-sm'><Link href='/safety'>Safety</Link></li>
                             <li className='text-white border-b-1 border-zinc-600 py-2 text-sm'><Link href='/faqs'>FAQs</Link></li>
                         </ul>
                     </div>
@@ -41,7 +51,7 @@ export default function Footer() {
                             <li className='text-white border-b-1 border-zinc-600 py-2 text-sm'><Link href='/help'>Help</Link></li>
                             <li className='text-white border-b-1 border-zinc-600 py-2 text-sm'><Link href='/contacts'>Contact Us</Link></li>
                         </ul>
-                        <Image src={'/images/apple-store.png'} width={150} height={100} alt={'apple'}  className='py-4'/>
+                        <Image src={'/images/apple-store.png'} width={150} height={100} alt={'apple'} className='py-4' />
                         <Image src={'/images/google-store.png'} width={150} height={100} alt={'apple'} />
                     </div>
                 </div>
