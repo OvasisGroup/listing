7bc27f8e32cfb33d48af9b2037f436446aa306d5
thekimpeople <thekimpeople@gmail.com>
2025-05-06 23:00:31 +0300

    payments

--- src/app/(mainLayout)/post-job/page.tsx
+++ src/app/(mainLayout)/post-job/page.tsx
@@ -1,12 +1,10 @@
 import CreateListingForm from '@/components/general/listings/CreateListings'
-import PaymentsList from '@/components/general/listings/Payments'
 import React from 'react'
 
 export default function PostJob() {
   return (
     <div>
       <CreateListingForm/>
-      <PaymentsList/>
     </div>
   )
 }
